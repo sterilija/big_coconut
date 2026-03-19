d9a2627a63a8323b032bf87214b60b00ede77a7f
Ilya <joint353@gmail.com>
2026-03-19 19:20:00 +0700

    added movies api handle

---
+++ api/movies_api.py
@@ -0,0 +1,49 @@
+from requester.requester import CustomRequester
+from models.model import CreateMovieDto, FindMoviesQuery
+from constants import MOVIES_ENDPOINT
+
+class MoviesAPI(CustomRequester):
+    def __init__(self, session, base_url, headers):
+        super().__init__(
+            session=session,
+            base_url=base_url,
+            headers=headers
+        )
+
+    def create_movie(self, data_json: CreateMovieDto):
+        return self.send_request(
+            method="POST",
+            expect_status=201,
+            endpoint= f"{self.base_url}/{MOVIES_ENDPOINT}",
+            data_json=data_json,
+        )
+
+    def get_movies_list(self, query: FindMoviesQuery):
+        return self.send_request(
+            method="GET",
+            expect_status=200,
+            endpoint=f"{self.base_url}/{MOVIES_ENDPOINT}",
+            query=query
+        )
+
+    def get_movie(self, movie_id):
+        return self.send_request(
+            method="GET",
+            expect_status=200,
+            endpoint=f"{self.base_url}/{MOVIES_ENDPOINT}/{movie_id}",
+        )
+
+    def delete_movie(self, movie_id):
+        return self.send_request(
+            method="DELETE",
+            expect_status=200,
+            endpoint=f"{self.base_url}/{MOVIES_ENDPOINT}/{movie_id}",
+        )
+
+    def edit_movie(self, movie_id, new_data_json: CreateMovieDto):
+        return self.send_request(
+            method="PATCH",
+            expect_status=200,
+            endpoint=f"{self.base_url}/{MOVIES_ENDPOINT}/{movie_id}",
+            data_json=new_data_json
+        )
