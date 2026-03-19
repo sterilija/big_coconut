fe31299fb8759fb751aa6fce357032b75bba7268
Ilya <joint353@gmail.com>
2026-03-19 20:17:46 +0700

    added auth api

---
+++ api/auth_api.py
@@ -0,0 +1,22 @@
+from requester.requester import CustomRequester
+from api.constants import REGISTER_ENDPOINT, LOGIN_ENDPOINT
+
+class AuthAPI(CustomRequester):
+    def __init__(self, session, base_url, headers):
+        super().__init__(session=session, base_url=base_url, headers=headers)
+
+    def register_user(self, user_jsom, expect_status=201):
+        return self.send_request(
+            method="POST",
+            endpoint=REGISTER_ENDPOINT,
+            data_json=user_jsom,
+            expect_status=expect_status
+        )
+
+    def login_user(self, login_json, expect_status=201):
+        return  self.send_request(
+            method='POST',
+            endpoint=LOGIN_ENDPOINT,
+            data_json=login_json,
+            expect_status=expect_status
+        )
