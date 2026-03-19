32065a1f39fbfc947265989422ec97c8047867d4
Ilya <joint353@gmail.com>
2026-03-19 17:43:00 +0700

    base project structure

---
+++ constants.py
@@ -0,0 +1,8 @@
+DEFAULT_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+BASE_URL = "https://api.dev-cinescope.coconutqa.ru/"
+
+MOVIES_ENDPOINT = "/movies"
